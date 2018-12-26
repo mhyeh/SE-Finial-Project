3bdc2d49e58c4be6897c1a5ab11f481d12761f43
BlackKite0206233 <samuel991640@gmail.com>
2018-12-26 15:38:21 +0800

    Fix some bugs

--- src/Project/models/Connection.js
+++ src/Project/models/Connection.js
@@ -1,5 +1,5 @@
 import * as mysql from 'mysql'
 import * as mongodb from 'mongodb'
 
-export const MySQL = mysql.createConnection(require('../../../configs/mysql.config.json'))
-export const MongoDBClient = mongodb.MongoClient
+export const MySQL   = mysql.createConnection(require('../../../configs/mysql.config.json'))
+export const MongoDB = await mongodb.MongoClient.connect(require('../../../configs/mongoDB.config.json').url)
--- src/Project/models/MongoDB.js
+++ src/Project/models/MongoDB.js
@@ -4,9 +4,14 @@ export default class Model {
     constructor(table) {
         this.connection = MongoDB
         this.table      = table
-        this.url        = 'mongodb://localhost:27017/sefin'
-        this.projStr   = { '_id': 0 }
+        this.projStr    = {}
         this.whereStr   = {}
+        this.compareOP  = {
+            ">":  "$gt",
+            ">=": "$gte",
+            "<":  "lt",
+            "<=": "lte"
+        }
     }
 
     select() {
@@ -37,79 +42,67 @@ export default class Model {
         if (arguments.length === 2) {
             this.whereStr[arguments[0]] = arguments[1]
         } else {
-            this.whereStr[arguments[0]] = `\\${arguments[2]}\\`
+            if (arguments[1] === 'like') {
+                this.whereStr[arguments[0]] = `/${arguments[2]}/`
+            } else {
+                if (!this.whereStr[arguments[0]]) {
+                    this.whereStr[arguments[0]] = {}
+                }
+                this.whereStr[arguments[0]][this.compareOP[arguments[1]]] = arguments[2]
+            }
         }
     }
 
     query() {
         return new Promise((resolve, reject) => {
-            this.MongoDB.connect(this.url, (err, db) => {
-                if (err) {
-                    throw err
-                }
-                db.collection(this.table).find(whereStr, projStr, (err, res) => {
-                    if (err) {
-                        reject(err)
-                        return
-                    }
-                    resolve(res)
-                })
-                db.close()
+            this.connection.collection(this.table).find(this.whereStr, this.projStr).then(res => {
+                this.flush()
+                resolve(res)
+            }).catch(err => {
+                this.flush()
+                reject(err)
             })
         })
     }
 
     insert(data) {
         return new Promise((resolve, reject) => {
-            this.MongoDB.connect(this.url, (err, db) => {
-                if (err) {
-                    throw err
-                }
-                db.collection(this.table).insertOne(data, (err, res) => {
-                    if (err) {
-                        reject(err)
-                        return
-                    }
-                    resolve(res)
-                })
-                db.close()
+            this.connection.collection(this.table).insertOne(data).then(res => {
+                this.flush()
+                resolve(res)
+            }).catch(err => {
+                this.flush()
+                reject(err)
             })
         })
     }
 
     update(data) {
         return new Promise((resolve, reject) => {
-            this.MongoDB.connect(this.url, (err, db) => {
-                if (err) {
-                    throw err
-                }
-                db.collection(this.table).updateOne(this.whereStr, data, (err, res) => {
-                    if (err) {
-                        reject(err)
-                        return
-                    }
-                    resolve(res)
-                })
-                db.close()
+            this.connection.collection(this.table).updateOne(this.whereStr, data).then(res => {
+                this.flush()
+                resolve(res)
+            }).catch(err => {
+                this.flush()
+                reject(err)
             })
         })
     }
 
     del() {
         return new Promise((resolve, reject) => {
-            this.MongoDB.connect(this.url, (err, db) => {
-                if (err) {
-                    throw err
-                }
-                db.collection(this.table).deleteMany(this.whereStr, (err, res) => {
-                    if (err) {
-                        reject(err)
-                        return
-                    }
-                    resolve(res)
-                })
-                db.close()
+            this.connection.collection(this.table).deleteMany(this.whereStr).then(res => {
+                this.flush()
+                resolve(res)
+            }).catch(err => {
+                this.flush()
+                reject(err)
             })
         })
+    }
+
+    flush() {
+        this.whereStr = {}
+        this.projStr  = {}
     }
 }
